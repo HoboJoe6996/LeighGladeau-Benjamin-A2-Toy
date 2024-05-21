9f356330b4afe4adcea0d93d6307211909c187c1
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-05-21 10:19:05 -0400

    Files compiled

---
+++ LeighGladeau-Benjamin-A2-Toy/LeighGladeau-Benjamin-A2-Toy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeighGladeau_Benjamin_A2_Toy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
